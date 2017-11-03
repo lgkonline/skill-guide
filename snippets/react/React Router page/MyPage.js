5ffe5f7d20fc3ee8e81ebea0b809a5eec92f9492
Lars Gerrit Kliesing <lgk@patorg.org>
2017-11-03 15:35:54 +0100

    Added snippets

---
+++ snippets/react/React Router page/MyPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
+class MyPage extends React.Component {
+    render() {
+        return (
+            <div />
+        );
+    }
+}
+
+MyPage.propTypes = {
+    history: PropTypes.object.isRequired
+};
+
+export default MyPage;
