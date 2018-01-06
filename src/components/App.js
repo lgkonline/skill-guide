a639617c25bc885ec892b1ee78e0ce2ff7b0705c
Lars / LGK <lgk@live.de>
2018-01-06 22:55:39 +0100

    installed lgk package

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Router, Route, Link } from "react-router-dom";
 import createHistory from "history/createHashHistory";
+import { LgkPillComponent } from "lgk";
 
 import HomePage from "../pages/HomePage";
 import GuidePage from "../pages/GuidePage";
@@ -13,6 +14,8 @@ class App extends React.Component {
         return (
             <Router history={routeHistory}>
                 <div style={{ display: "flex", minHeight: "100vh", flexDirection: "column" }}>
+                    <LgkPillComponent />
+
                     <Route exact path="/" component={HomePage} />
                     <Route path="/guide/:user/:repo/:step?" component={GuidePage} />
                     <Route path="/snippets" component={SnippetsPage} />
